1f1429f56049eb8599091b06aa5a7390a236fa8a
kapcina69 <lukasmolovicpp@gmail.com>
2025-04-28 13:42:53 +0200

    Azuriranje vremena preko BT

--- src/lcd_display.c
+++ src/lcd_display.c
@@ -1,9 +1,8 @@
 #include "lcd_display.h"
 #include "rtc_driver.h"
-#include <stdlib.h>  // za rand()
+#include <stdlib.h>
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
-#include <zephyr/kernel.h>
 #include <lvgl.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/display.h>
@@ -12,28 +11,26 @@
 
 LOG_MODULE_REGISTER(display_app);
 
-// Definicije za prikaze
 typedef enum {
     DISPLAY_HR,
     DISPLAY_STEPS,
     DISPLAY_TIME,
-    DISPLAY_COUNT  // Broj prikaza
+    DISPLAY_COUNT
 } display_view_t;
 
-// Globalne promenljive
- lv_obj_t *hr_label = NULL;
- lv_obj_t *steps_label = NULL;
+lv_obj_t *hr_label = NULL;
+lv_obj_t *steps_label = NULL;
 static lv_obj_t *time_label = NULL;
 static display_view_t current_view = DISPLAY_HR;
 static lv_timer_t *display_timer = NULL;
+static lv_timer_t *time_update_timer = NULL;  // Dodat tajmer za ažuriranje vremena
 
-// Prototipovi funkcija
 void init_lcd_display(void);
 void update_lcd_display(uint32_t hr, uint32_t steps);
 void update_time_display(void);
 void switch_display_view(lv_timer_t *timer);
+void update_time_callback(lv_timer_t *timer);  // Nova callback funkcija
 
-// Inicijalizacija displeja
 void init_lcd_display(void)
 {
     const struct device *display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
@@ -42,12 +39,10 @@ void init_lcd_display(void)
         return;
     }
 
-    // Stil za font
     static lv_style_t style;
     lv_style_init(&style);
     lv_style_set_text_font(&style, &lv_font_montserrat_14);
 
-    // Kreiranje labela - svi su centrirani na ekranu
     hr_label = lv_label_create(lv_scr_act());
     lv_obj_add_style(hr_label, &style, 0);
     lv_obj_align(hr_label, LV_ALIGN_CENTER, 0, 0);
@@ -65,14 +60,13 @@ void init_lcd_display(void)
     lv_label_set_text(time_label, "Time: --");
     lv_obj_add_flag(time_label, LV_OBJ_FLAG_HIDDEN);
 
-    // Kreiranje tajmera za promenu prikaza (3000 ms = 3 sekunde)
-    display_timer = lv_timer_create(switch_display_view, 3000, NULL);
+    display_timer = lv_timer_create(switch_display_view, 8000, NULL);
+    time_update_timer = lv_timer_create(update_time_callback, 1000, NULL);  // Ažuriraj vreme svake sekunde
 
     lv_task_handler();
     display_blanking_off(display_dev);
 }
 
-// Ažuriranje prikaza srca
 void update_lcd_display(uint32_t hr, uint32_t steps)
 {
     char hr_text[32];
@@ -91,12 +85,15 @@ void update_lcd_display(uint32_t hr, uint32_t steps)
     lv_task_handler();
 }
 
-#include <zephyr/drivers/rtc.h>
-#include <zephyr/sys/timeutil.h>
+// Nova funkcija koja se poziva svake sekunde da ažurira vreme
+void update_time_callback(lv_timer_t *timer)
+{
+    rtc_update_time_label(time_label);
+    if (current_view == DISPLAY_TIME) {
+        lv_task_handler();  // Ažuriraj prikaz samo ako je vreme trenutno prikazano
+    }
+}
 
-
-
-// Promena prikaza - poziva se na svaki tajmer
 void switch_display_view(lv_timer_t *timer)
 {
     // Sakrij trenutni prikaz
@@ -117,7 +114,7 @@ void switch_display_view(lv_timer_t *timer)
     // Pređi na sledeći prikaz
     current_view = (current_view + 1) % DISPLAY_COUNT;
 
-    // Prikaži novi prikaz i ažuriraj ga ako je potrebno
+    // Prikaži novi prikaz
     switch (current_view) {
         case DISPLAY_HR:
             lv_obj_clear_flag(hr_label, LV_OBJ_FLAG_HIDDEN);
@@ -126,10 +123,10 @@ void switch_display_view(lv_timer_t *timer)
             lv_obj_clear_flag(steps_label, LV_OBJ_FLAG_HIDDEN);
             break;
         case DISPLAY_TIME:
-            rtc_update_time_label(time_label);
             lv_obj_clear_flag(time_label, LV_OBJ_FLAG_HIDDEN);
             break;
         default:
             break;
     }
+    lv_task_handler();
 }
